d2209bc5a478aeaeeec1522120eb16bea532c124
Amadeus Darren Leander <41075353+adarrenleander@users.noreply.github.com>
2019-06-19 20:01:03 +0700

    Finalized project

--- js/card-script.js
+++ js/card-script.js
@@ -4,13 +4,13 @@ var confPass = document.forms['myForm']['txtConfPass'];
 var address = document.forms['myForm']['txtAddress'];
 var rbMale = document.forms['myForm']['rbMale'];
 var rbFemale = document.forms['myForm']['rbFemale'];
-var dob = document.forms['myForm']['dtpDOB'];
+var dob = document.forms['myForm']['txtDOB'];
 
 function checkEmail() {
     var error = document.getElementById('errEmail');
 
     if (email.value == "") {
-        error.innerHTML = "Email must be filled in!";
+        error.innerHTML = "Email must be filled!";
         return false;
     }
     else if (email.value.search('@') == -1 || email.value.search('.') == -1) {
@@ -65,7 +65,7 @@ function checkPassword() {
     var error = document.getElementById("errPass");
 
     if (password.value == "") {
-        error.innerHTML = "Password must be filled in!";
+        error.innerHTML = "Password must be filled!";
         return false;
     }
     else if (password.value.length < 6 || password.value.length > 15) {
@@ -90,7 +90,7 @@ function checkConfPassword() {
     var error = document.getElementById("errConfPass");
 
     if (confPass.value == "") {
-        error.innerHTML = "Confirm Password must be filled in!";
+        error.innerHTML = "Confirm Password must be filled!";
         return false;
     }
     else if (confPass.value != password.value) {
@@ -107,7 +107,7 @@ function checkAddress() {
     var error = document.getElementById("errAddress");
 
     if (address.value == "") {
-        error.innerHTML = "Address must be filled in!";
+        error.innerHTML = "Address must be filled!";
         return false;
     }
     else {
@@ -129,19 +129,79 @@ function checkGender() {
     }
 }
 
-function checkDOB() {
-    var error = document.getElementById("errDOB");
-
-    // if (dob.value == "2019-06-19") {
-    //     error.innerHTML = "Date of Birth must be chosen!";
-    //     return false;
-    // }
-    // else {
-    //     error.innerHTML = "";
-    //     return true;
-    // }
+function checkDateDigits() {
+    var i;
+    for (i=0;i<dob.value.length;i++) {
+        if (i != 4 && i != 7) {
+            if (dob.value.charCodeAt(i) < 48 || dob.value.charCodeAt(i) > 57) {
+                return false;
+            }
+        }
+    }
 
     return true;
+}
+
+function checkDateRange() {
+    var parts = dob.value.split('/');
+    var years = parseInt(parts[0]);
+    var months = parseInt(parts[1]);
+    var days = parseInt(parts[2]);
+
+    if (years > 2019) {
+        return false;
+    }
+
+    if (months > 12) {
+        return false;
+    }
+    
+    if (months == 2) {
+        if (days > 29) {
+            return false;
+        }
+    }
+    else if (months == 1 || months == 3 || months == 5 || months == 7 || months == 8 || months == 10 || months == 12) {
+        if (days > 31) {
+            return false;
+        }
+    }
+    else if (months == 4 || months == 6 || months == 9 || months == 11) {
+        if (days > 30) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function checkDOB() {
+    var error = document.getElementById("errDOB");
+    
+    if (dob.value == "") {
+        error.innerHTML = "Date of Birth must be filled!";
+        return false;
+    }
+    else if (dob.value.length != 10) {
+        error.innerHTML = "Date of Birth format is incorrect!";
+        return false;
+    }
+    else if (dob.value.indexOf('/') != 4 || dob.value.indexOf('/', 5) != 7) {
+        error.innerHTML = "Date of Birth format is incorrect!";
+        return false;
+    }
+    else if (!checkDateDigits()) {
+        error.innerHTML = "Date of Birth is invalid!";
+        return false;
+    }
+    else if (!checkDateRange()) {
+        error.innerHTML = "Date of Birth is invalid!";
+        return false;
+    }
+    else {
+        error.innerHTML = "";
+        return true;
+    }
 }
 
 function validate() {
